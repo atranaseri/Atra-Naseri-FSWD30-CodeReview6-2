b818fc24903b7ceb2741e6c02d58dc4cee1a6f1f
atra naseri <atra.naseri@yahoo.com>
2018-01-13 17:34:43 +0100

    first commit

---
+++ Code-review6.js
@@ -0,0 +1,88 @@
+//----------------------------------------------
+
+//---- Information for Music are here ----------------------//
+var Music1 ={
+	"Titel":"Dylan",
+	"Author":"Bob Dylan",
+	"Genre":"Rock",
+	"Minutes":34
+};
+document.getElementById('M1').innerHTML=("Titel: "+ Music1.Titel);
+document.getElementById('M2').innerHTML=("Author: "+ Music1.Author);
+document.getElementById('M3').innerHTML=("Genre: "+ Music1.Genre);
+document.getElementById('M6').innerHTML=("Minutes: "+ Music1.Minutes);
+//----------------------------------------------
+var Music2 ={
+	"Titel":"DiamonLife",
+	"Author":"Sade",
+	"Genre":"Smooth Soul",
+	"Publisher":"Epic",
+	"Rating":5,
+	"Minutes":45
+};
+document.getElementById('M7').innerHTML=("Titel: "+ Music2.Titel);
+document.getElementById('M8').innerHTML=("Author: "+ Music2.Author);
+document.getElementById('M9').innerHTML=("Genre: "+ Music2.Genre);
+document.getElementById('M10').innerHTML=("Publisher: "+ Music2.Publisher);
+document.getElementById('M11').innerHTML=("Rating: "+ Music2.Rating + ' <span class="glyphicon glyphicon-star"></span>' +' <span class="glyphicon glyphicon-star"></span>' + ' <span class="glyphicon glyphicon-star"></span>' + ' <span class="glyphicon glyphicon-star"></span>' + ' <span class="glyphicon glyphicon-star"></span>');
+document.getElementById('M12').innerHTML=("Minutes: "+ Music2.minutes);
+//----------------------------------------------
+var Music3 ={
+	"Titel":"The Miracle",
+	"Author":"Queen",
+	"Genre":"Rock",
+	"Publisher":"David Richards",
+	"Rating":3,
+	"Minutes":40
+};
+document.getElementById('M13').innerHTML=("Titel: "+ Music3 .Titel);
+document.getElementById('M14').innerHTML=("Author: "+ Music3 .Author);
+document.getElementById('M15').innerHTML=("Genre: "+ Music3 .Genre);
+document.getElementById('M16').innerHTML=("Publisher: "+ Music3 .Publisher);
+document.getElementById('M17').innerHTML=("Rating: "+ Music3 .Rating + ' <span class="glyphicon glyphicon-star"></span>' + ' <span class="glyphicon glyphicon-star"></span>' + ' <span class="glyphicon glyphicon-star"></span>');
+document.getElementById('M18').innerHTML=("Minutes: "+ Music3 .Minutes);
+//----------------------------------------------
+
+//---- Information for Movies are here ----------------------------------------//
+var Movie1 ={
+	"Titel":"City Lights",
+	"Author":"Charle Chaplin",
+	"Genre":"Romance Comedy",
+	"Publisher":"United Artist",
+	"Minutes":87
+};
+document.getElementById('D1').innerHTML=("Titel: "+ Movie1.Titel);
+document.getElementById('D2').innerHTML=("Author: "+ Movie1.Author);
+document.getElementById('D3').innerHTML=("Genre: "+ Movie1.Genre);
+document.getElementById('D4').innerHTML=("Publisher: "+ Movie1.Publisher);
+document.getElementById('D5').innerHTML=("Minutes: "+ Movie1.Minutes);
+
+//----------------------------------------------
+var Movie2 ={
+	"Titel":"Gone with Wind",
+	"Author":"Margaret Mitchel",
+	"Genre":"Civilwar Romance",
+	"Publisher":"January 30 1953 (Austria)",
+	"Minutes":238
+};
+document.getElementById('D6').innerHTML=("Titel: "+ Movie2.Titel);
+document.getElementById('D7').innerHTML=("Author: "+ Movie2.Author);
+document.getElementById('D8').innerHTML=("Genre: "+ Movie2.Genre);
+document.getElementById('D9').innerHTML=("Publisher: "+ Movie2.Publisher);
+document.getElementById('D10').innerHTML=("Minutes: "+ Movie2.Minutes);
+
+//----------------------------------------------
+var Movie3 ={
+	"Titel":"Lawrence of Arabia",
+	"Author":"T.E Lawrence",
+	"Genre":"Historical Drama",
+	"Publisher":"Columbia Picture",
+	"Minutes":222
+};
+document.getElementById('D11').innerHTML=("Titel: "+ Movie3.Titel);
+document.getElementById('D12').innerHTML=("Author: "+ Movie3.Author);
+document.getElementById('D13').innerHTML=("Genre: "+ Movie3.Genre);
+document.getElementById('D14').innerHTML=("Publisher: "+ Movie3.Publisher);
+document.getElementById('D15').innerHTML=("Minutes: "+ Movie3.Minutes);
+
+//----------------------------------------------
---
+++ myscript.js
@@ -0,0 +1,177 @@
+//------------ I do know why this page is some how not connected to my HTML,--//
+//--I had to write all these in my HTML file but here it#s easy to read---//
+
+//---- Information for Books are here --//
+var Book1 ={
+	"Titel":"Woodwalkers",
+	"Author":"Katja Brandis",
+	"Genre":"belletristik",
+	"Publisher":"Bild",
+	"Rating":5,
+	"Weight":1
+};
+document.getElementById('B1').innerHTML=("Titel: "+ Book1.Titel);
+document.getElementById('B2').innerHTML=("Author: "+ Book1.Author);
+document.getElementById('B3').innerHTML=("Genre: "+ Book1.Genre);
+document.getElementById('B4').innerHTML=("Publisher: "+ Book1.Publisher);
+document.getElementById('B5').innerHTML=("Rating: "+ Book1.Rating);
+document.getElementById('B6').innerHTML=("Weight: "+ Book1.Weight);
+//----------------------------------------------
+var Book2 ={
+	"Titel":"Foundation",
+	"Author":"Isaac Asimov",
+	"Genre":"fantasy science fiction",
+	"Publisher":"Heyne",
+	"Rating":3,
+	"Weight":2 "kg"
+};
+document.getElementById('B7').innerHTML=("Titel: "+ Book2.Titel);
+document.getElementById('B8').innerHTML=("Author: "+ Book2.Author);
+document.getElementById('B9').innerHTML=("Genre: "+ Book2.Genre);
+document.getElementById('B10').innerHTML=("Publisher: "+ Book2.Publisher);
+document.getElementById('B11').innerHTML=("Rating: "+ Book2.Rating);
+document.getElementById('B12').innerHTML=("Weight: "+ Book2.Weight);
+//----------------------------------------------
+var Book3 ={
+	"Titel":"Modesty",
+	"Author":"Peter o'Donnell",
+	"Genre":"mystery",
+	"Publisher":"Titan Book"
+	
+};
+document.getElementById('B13').innerHTML=("Titel: "+ Book3.Titel);
+document.getElementById('B14').innerHTML=("Author: "+ Book3.Author);
+document.getElementById('B15').innerHTML=("Genre: "+ Book1.Genre);
+document.getElementById('B16').innerHTML=("Publisher: "+ Book3.Publisher);
+document.getElementById('B17').innerHTML=("Rating: "+ Book3.Rating);
+document.getElementById('B18').innerHTML=("Weight: "+ Book3.Weight);
+//----------------------------------------------
+
+//---- Information for Comics Book are here ----------------------------------------//
+var Comics1 ={
+	"Titel":"The Sandman",
+	"Author":"Neil Gaiman",
+	"Genre":"Fantay",
+	"Publisher":"vertigo",
+	"Rating":5,
+	"Weight":1
+};
+document.getElementById('C1').innerHTML=("Titel: "+ Comics1.Titel);
+document.getElementById('C2').innerHTML=("Author: "+ Comics1.Author);
+document.getElementById('C3').innerHTML=("Genre: "+Comics1.Genre);
+document.getElementById('C4').innerHTML=("Publisher: "+ Comics1.Publisher);
+
+//----------------------------------------------
+var Comics2 ={
+	"Titel":"Spiderman",
+	"Author":"Stan Lee",
+	"Genre":"fsuperhero fiction",
+	"Publisher":"Marvel Comics",
+	
+};
+document.getElementById('C5').innerHTML=("Titel: "+ Comics2 .Titel);
+document.getElementById('C6').innerHTML=("Author: "+ Comics2 .Author);
+document.getElementById('C7').innerHTML=("Genre: "+ Comics2 .Genre);
+document.getElementById('C8').innerHTML=("Publisher: "+ Comics2 .Publisher);
+
+//----------------------------------------------
+var Comics3 ={
+	"Titel":"WolveRine",
+	"Author":"len wein",
+	"Genre":"superhero fiction",
+	"Publisher":"Marvel Comics"
+	
+};
+document.getElementById('C9').innerHTML=("Titel: "+ Comics3 .Titel);
+document.getElementById('C10').innerHTML=("Author: "+ Comics3 .Author);
+document.getElementById('C11').innerHTML=("Genre: "+ Comics3 .Genre);
+document.getElementById('C12').innerHTML=("Publisher: "+ Comics3 .Publisher);
+//----------------------------------------------
+
+//---- Information for Music are here ----------------------//
+var Music1 ={
+	"Titel":"Dylan",
+	"Author":"Bob Dylan",
+	"Genre":"Rock",
+	"Minutes":34
+};
+document.getElementById('M1').innerHTML=("Titel: "+ Music1.Titel);
+document.getElementById('M2').innerHTML=("Author: "+ Music1.Author);
+document.getElementById('M3').innerHTML=("Genre: "+ Music1.Genre);
+document.getElementById('M6').innerHTML=("Minutes: "+ Music1.Minutes);
+//----------------------------------------------
+var Music2 ={
+	"Titel":"DiamonLife",
+	"Author":"Sade",
+	"Genre":"Smooth Soul",
+	"Publisher":"Epic",
+	"Rating":5,
+	"Minutes":45
+};
+document.getElementById('M7').innerHTML=("Titel: "+ Music2.Titel);
+document.getElementById('M8').innerHTML=("Author: "+ Music2.Author);
+document.getElementById('M9').innerHTML=("Genre: "+ Music2.Genre);
+document.getElementById('M10').innerHTML=("Publisher: "+ Music2.Publisher);
+document.getElementById('M11').innerHTML=("Rating: "+ Music2.Rating);
+document.getElementById('M12').innerHTML=("Minutes: "+ Music2.minutes);
+//----------------------------------------------
+var Music3 ={
+	"Titel":"The Miracle",
+	"Author":"Queen",
+	"Genre":"Rock",
+	"Publisher":"David Richards",
+	"Rating":3/5,
+	"Minutes":40
+};
+document.getElementById('M13').innerHTML=("Titel: "+ Music3 .Titel);
+document.getElementById('M14').innerHTML=("Author: "+ Music3 .Author);
+document.getElementById('M15').innerHTML=("Genre: "+ Music3 .Genre);
+document.getElementById('M16').innerHTML=("Publisher: "+ Music3 .Publisher);
+document.getElementById('M17').innerHTML=("Rating: "+ Music3 .Rating);
+document.getElementById('M18').innerHTML=("Minutes: "+ Music3 .Minutes);
+//----------------------------------------------
+
+//---- Information for Movies are here ----------------------------------------//
+var Movie1 ={
+	"Titel":"City Lights",
+	"Author":"Charle Chaplin",
+	"Genre":"Romance Comedy",
+	"Publisher":"United Artist",
+	"Minutes":87
+};
+document.getElementById('D1').innerHTML=("Titel: "+ Movie1.Titel);
+document.getElementById('D2').innerHTML=("Author: "+ Movie1.Author);
+document.getElementById('D3').innerHTML=("Genre: "+ Movie1.Genre);
+document.getElementById('D4').innerHTML=("Publisher: "+ Movie1.Publisher);
+document.getElementById('D5').innerHTML=("Minutes: "+ Movie1.Minutes);
+
+//----------------------------------------------
+var Movie2 ={
+	"Titel":"Gone with Wind",
+	"Author":"Margaret Mitchel",
+	"Genre":"Civilwar Romance",
+	"Publisher":"January 30 1953 (Austria)",
+	"Minutes":238
+};
+document.getElementById('D6').innerHTML=("Titel: "+ Movie2.Titel);
+document.getElementById('D7').innerHTML=("Author: "+ Movie2.Author);
+document.getElementById('D8').innerHTML=("Genre: "+ Movie2.Genre);
+document.getElementById('D9').innerHTML=("Publisher: "+ Movie2.Publisher);
+document.getElementById('D10').innerHTML=("Minutes: "+ Movie2.Minutes);
+
+//----------------------------------------------
+var Movie3 ={
+	"Titel":"Lawrence of Arabia",
+	"Author":"T.E Lawrence",
+	"Genre":"Historical Drama",
+	"Publisher":"Columbia Picture",
+	"Minutes":222
+};
+document.getElementById('D11').innerHTML=("Titel: "+ Movie3.Titel);
+document.getElementById('D12').innerHTML=("Author: "+ Movie3.Author);
+document.getElementById('D13').innerHTML=("Genre: "+ Movie3.Genre);
+document.getElementById('D14').innerHTML=("Publisher: "+ Movie3.Publisher);
+document.getElementById('D15').innerHTML=("Minutes: "+ Movie3.Minutes);
+
+//----------------------------------------------
+
